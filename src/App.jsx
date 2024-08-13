629bf0c3a3a4d8d44074b06a9a94571c628a2fb4
pnh1412 <phamnguyenhau1412@gmail.com>
2024-08-14 04:26:23 +0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
+import './App.css'
+import { NavBar } from "./components/Navbar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
